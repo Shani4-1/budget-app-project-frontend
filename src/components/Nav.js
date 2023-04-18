2c398e388aed17be8c893677a02fde236738c05a
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-18 11:04:54 -0400

    Completed Nav and functionality.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
-    return "I'm in the Nav"
-}
+    return (
+        <nav>
+            <Link to="/">
+                <h1>Budget App</h1>
+            </Link>
+            <button>
+            <Link to="/transactions">View Transactions</Link>
+            </button>
+            <button>
+                <Link to="/transactions/new">New Transaction</Link>
+            </button>
+        </nav>
+    )};
+
+
 
 export default Nav;
