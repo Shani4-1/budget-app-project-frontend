daaf1deb5ca668c0212e32702f219817fcd2af86
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-18 11:57:54 -0400

    Completed Transaction component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,14 @@
-const Transaction = () => {
-    return "I'm a Transaction";
+const Transaction = ({transaction, index}) => {
+    return (
+        <tr>
+            <td>{transaction.date}</td>
+            <td><a href={`/transactions/${index}`}>View</a></td>
+            <td><a href={`/transactions/${index}/edit`}>Edit</a></td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.deposit ? "Yes" : "No"}</td>
+            <td>{transaction.from}</td>
+        </tr>
+    );
 };
 
 
