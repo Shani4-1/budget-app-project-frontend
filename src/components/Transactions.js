5917db674f325085546aa5a92a00c2c1cecfb2d0
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-19 01:23:24 -0400

    fixed table functionality  added Account Balance

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 import Transaction from "./Transaction.js";
 
 const URL = process.env.REACT_APP_API_URL;
@@ -9,7 +10,6 @@ const Transactions = () => {
 
   useEffect(() => {
     axios.get(`${URL}/transactions`).then((res) => {
-      
       setTransactions(res.data);
     });
   }, []);
@@ -17,6 +17,21 @@ const Transactions = () => {
   return (
     <div className="Transactions">
       <section>
+        <h1>
+          Account Balance: $
+          {(() => {
+            let total = 0;
+            transactions.forEach((transaction) => {
+              if (transaction.deposit) {
+                total += Number(transaction.amount);
+              } else {
+                total -= Number(transaction.amount);
+              }
+            });
+            return Number(total.toFixed(2));
+          })()}
+        </h1>
+
         <table>
           <thead>
             <tr>
@@ -26,15 +41,15 @@ const Transactions = () => {
             </tr>
           </thead>
           <tbody>
-            {transactions.map((transaction, index) => {
-              return (
+            {transactions
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .map((transaction, index) => (
                 <Transaction
                   key={index}
                   transaction={transaction}
                   index={index}
                 />
-              );
-            })}
+              ))}
           </tbody>
         </table>
       </section>
